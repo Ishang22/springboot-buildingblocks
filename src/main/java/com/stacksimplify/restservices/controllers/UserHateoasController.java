d7bf7b2ca22561f7f558cd3fbbd5f831256f2652
ishan <officialgarg1@gmail.com>
2021-06-09 21:35:19 +0530

    ishannn

--- src/main/java/com/stacksimplify/restservices/controllers/UserHateoasController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserHateoasController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.constraints.Min;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
